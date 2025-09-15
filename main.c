46a8fe14fbf867927252f2330387376de98c0bf5
Paul-hr1 <paul.huray@isen-ouest.yncrea.fr>
2025-09-15 14:42:11 +0200

    petit push

--- main.c
+++ main.c
@@ -1,1 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
 
+struct Cell {
+    struct Cell* adjacent_cells[4];
+};
+
+int main(void) {
+    // Initialisation avec une graine dépendant du temps.
+    srand(time(NULL));
+    printf("%d", rand() % 100); // Nombre aléatoire entre 0 et 99.
+}
